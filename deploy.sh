a5ccb1c27add60be23b56288ac37b6fd50c7c30f
kimsookyeom <ikks06luck@naver.com>
2018-06-15 15:02:30 +0900

    init

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+echo 'Running npm install & npm build'
+npm set progress=false && npm install -s --no-progress && npm run build
+echo 'Done...'
+
+echo 'Format index.html as Jinja template'
+python3 format_index_html.py
+echo 'Done...'
+
+echo 'Install python modules'
+pip install -r requirements.txt
+echo 'Done...'
+
+echo 'Collect static'
+python3 manage.py collectstatic --noinput
+echo 'Done...'
+
+export PORT=8000
+echo 'Server runnning on port ' $PORT
+python3 manage.py runserver
