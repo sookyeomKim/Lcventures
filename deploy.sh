f9641b79d6877301d8994fa665eff3865cd391ad
kimsookyeom <ikks06luck@naver.com>
2018-07-05 17:14:47 +0900

    modify aws codedeploy setting

--- deploy.sh
+++ deploy.sh
@@ -9,21 +9,17 @@ echo 'Done...'
 
 
 echo 'Running npm install & npm build'
-npm set progress=false && npm install -s --no-progress && npm run build
+cd /home/ubuntu/Projects/Lcventures && npm set progress=false && npm install -s --no-progress && npm run build
 echo 'Done...'
 
 echo 'Format index.html as Jinja template'
-python3 format_index_html.py
+cd /home/ubuntu/Projects/Lcventures && python3 format_index_html.py
 echo 'Done...'
 
 echo 'Install python modules'
-pip install -r requirements.txt
+cd /home/ubuntu/Projects/Lcventures && pip install -r requirements.txt
 echo 'Done...'
 
 echo 'Collect static'
-python3 manage.py collectstatic --noinput
+cd /home/ubuntu/Projects/Lcventures && python3 manage.py collectstatic --noinput
 echo 'Done...'
-
-export PORT=8000
-echo 'Server runnning on port ' $PORT
-python3 manage.py runserver
